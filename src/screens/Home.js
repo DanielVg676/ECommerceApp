5fe718abc6313b1e14b7f596bc6fe93a56bd77e5
DANIEL <dv956543@gmail.com>
2025-03-18 22:11:50 -0600

    wedfewfew

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,16 @@
 import { React } from "react";
 import { View, Text } from 'react-native';
 
+const categorias = [
+    { id: '1', title:'Electronica', imagenUrl: ''},
+    { id: '2', title:'Ropa', imagenUrl: ''},
+    { id: '3', title:'Muebles', imagenUrl: ''},
+    { id: '4', title:'Juguetes', imagenUrl: ''},
+];
+
 const Home = () => {
     return (
+        
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
             <Text>Pantalla de Inicio</Text>
         </View>
